724b1ab050f517110473da1e1ce6b237fdbabafc
Samuel Webb <smarkwebb@gmail.com>
2024-06-25 17:43:46 +0100

    Created methods to generate grid

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 class Grid:
-    def __init__(self):
-        pass
+    def __init__(self, gridSize):
+        self.__grid = []
+        self.__gridSize = gridSize
+
+    def generateGrid(self):
+        for row in range(0, self.__gridSize):
+            self.__grid.append([])
+            for column in range(0, self.__gridSize):
+                self.__grid[row].append("-")
+
+    def outputGrid(self):
+        for row in range(0, self.__gridSize):
+            for column in range(0, self.__gridSize):
+                print(self.__grid[row][column], end="")
+            print("")
